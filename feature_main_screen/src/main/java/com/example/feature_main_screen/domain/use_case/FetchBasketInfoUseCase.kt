383e1a8971734e609ccf201465f169aea6711340
chuvalik <chuvalik.work@gmail.com>
2022-05-16 06:39:38 +0300

    app adapter delegates

--- feature_main_screen/src/main/java/com/example/feature_main_screen/domain/use_case/FetchBasketInfoUseCase.kt
+++ feature_main_screen/src/main/java/com/example/feature_main_screen/domain/use_case/FetchBasketInfoUseCase.kt
@@ -1,7 +1,5 @@
 package com.example.feature_main_screen.domain.use_case
 
-import com.example.core.utils.Resource
-import com.example.feature_main_screen.domain.model.BasketDomain
 import com.example.feature_main_screen.domain.repository.MainScreenRepository
 
 class FetchBasketInfoUseCase(
